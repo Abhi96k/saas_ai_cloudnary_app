b7abb16e93ab523c7b0f978efb5ed89240d7e8db
abhi@96k <abhisheknangare96k@gmail.com>
2024-10-15 15:22:06 +0530

    Add video upload page and update dependencies in package.json

---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoUploadPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Upload Video</h1>
+      <p>Upload your video here.</p>
+    </div>
+  );
+};
+
+export default VideoUploadPage;
