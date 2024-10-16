e5964f75f885ea89d43cf0a45aad4a2d093cac09
abhi@96k <abhisheknangare96k@gmail.com>
2024-10-16 19:01:04 +0530

    Add Next.js Image component for optimized image handling; update VideoCard and layout components

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -13,6 +13,8 @@ import {
   ImageIcon,
 } from "lucide-react";
 
+import Image from "next/image";
+
 const sidebarItems = [
   { href: "/home", icon: LayoutDashboardIcon, label: "Home Page" },
   { href: "/social-share", icon: Share2Icon, label: "Social Share" },
@@ -71,11 +73,13 @@ export default function AppLayout({
                 <>
                   <div className="avatar">
                     <div className="w-8 h-8 rounded-full">
-                      <img
+                      <Image
                         src={user.imageUrl}
                         alt={
                           user.username || user.emailAddresses[0].emailAddress
                         }
+                        width={32}
+                        height={32}
                       />
                     </div>
                   </div>
